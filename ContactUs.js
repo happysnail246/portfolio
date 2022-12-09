d7804afe059466985191ddb8365add18ed22beb9
happysnail246 <113464144+happysnail246@users.noreply.github.com>
2022-12-09 13:06:26 -0500

    Add files via upload

---
+++ ContactUs.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me!
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>Linkedin: {resumeData.linkedinId}
+                </h4>
+                <h4>Email: {resumeData.email}
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
